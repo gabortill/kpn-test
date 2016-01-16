cd233881c770e4b19cab528800afaab3b41d8b30
tillgabi <till.gabor@outlook.com>
2016-01-16 17:00:59 +0100

    added calculated values

--- src/app/kpn/kpn.controller.js
+++ src/app/kpn/kpn.controller.js
@@ -4,11 +4,26 @@
 	function KpnController(DataService) {
 		var vm = this;
 
+		function calcDiscountPrice(price, discount) {
+			return price - discount;
+		}
+
+		function addCalcValues(deviceOrigPrice, subsArray) {
+			subsArray.forEach(function(elem) {
+				elem.calc = {
+					deviceDiscPrice: calcDiscountPrice(deviceOrigPrice, elem.deviceDiscount),
+					subsDiscPrice: calcDiscountPrice(elem.subsPrice, elem.subsDiscount)
+				};
+			});
+		}
+
 		DataService
 			.getData()
 			.then(function (data) {
 				if (!data.isError) {
 					vm.product = data;
+
+					addCalcValues(data.deviceOrigPrice, data.subscriptions);
 				} else {
 					vm.isDataError = true;
 					vm.errorMsg = data.errorMsg;
